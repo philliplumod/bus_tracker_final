d85eaac34456bca9441a975dfc18014c7a90ee00
philliplumod <philliplumod@gmail.com>
2026-02-04 00:04:37 +0800

    fix: Update minSdk comment to clarify Google Maps requirement

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
         applicationId = "com.example.bus_tracker"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = flutter.minSdkVersion  // Google Maps requires minimum SDK 21
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
